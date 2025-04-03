619c75b5de97451b784a321f4690d556b3c49ee9
ovatusGMattos <gustavo.j.mattos1@gmail.com>
2025-04-03 14:41:25 -0300

    Add project files.

---
+++ Gestao-De-Equipamentos.ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestao_De_Equipamentos.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
